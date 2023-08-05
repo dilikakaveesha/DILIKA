fd7555a20301bc912784dbda00be644232816c02
dilikakaveesha <141518148+dilikakaveesha@users.noreply.github.com>
2023-08-06 01:25:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94727505437', 'me', true],
+  ['94727505437', 'shan', true], 
   [''] 
 ] //Number of owners
 
@@ -47,10 +47,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = '𝙳𝙸𝙻𝙸𝙺𝙰 𝚃𝙷𝙰𝚁𝚄𝙺𝚂𝙷𝙰𝙽'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.packname = '𝚃𝙷𝙰𝚁𝚄𝙺𝚂𝙷𝙰𝙽' 
+global.author = '@𝚃𝙷𝙰𝚁𝚄𝙺𝚂𝙷𝙰𝙽' 
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT-V1' 
